4094758e921f6a86104b17dd8a76300d30b0cb70
Daniel Smith <jellymann@gmail.com>
2014-10-26 14:49:32 +0200

    Fix nesting bug

--- lib/tag_remover.rb
+++ lib/tag_remover.rb
@@ -6,18 +6,26 @@ module TagRemover
     tags_to_remove = opts[:remove_tags] || []
 
     in_tag = nil
+    depth = 0
 
     input.each_line.reduce(0) do |i,line|
       puts i if i % 10_000 == 0
 
       if in_tag
         in_tag_str = tags_to_remove[in_tag]
-        in_tag = nil if line =~ /<\/#{in_tag_str}>/
+
+        if line =~ /<#{in_tag_str}>/
+          depth += 1
+        elsif line =~ /<\/#{in_tag_str}>/
+          depth -= 1
+          in_tag = nil if depth == 0
+        end
       else
         found_tag = false
         tags_to_remove.each_with_index do |tag,index|
           if line =~ /<#{tag}>/
             in_tag = index
+            depth = 1
             found_tag = true
             break
           elsif line =~ /<#{tag}\/>/
