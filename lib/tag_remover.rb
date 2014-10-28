250bbebc212b13c9ddeda2a6f422c58335388112
Daniel Smith <jellymann@gmail.com>
2014-10-28 17:48:39 +0200

    Handle multiple tags on one line

--- lib/tag_remover.rb
+++ lib/tag_remover.rb
@@ -2,43 +2,97 @@ require "tag_remover/version"
 
 module TagRemover
 
-  def self.process input, output, opts = {}
-    tags_to_remove = opts[:remove_tags] || []
+  class Worker
+    def initialize input, output, opts
+      @input = input
+      @output = output
+      @opts = opts
+    end
 
-    in_tag = nil
-    depth = 0
+    def perform
+      @tags_to_remove = @opts[:remove_tags] || []
 
-    input.each_line.each do |line|
-      if in_tag
-        in_tag_str = tags_to_remove[in_tag]
+      @in_tag = nil
+      @depth = 0
 
-        if line =~ /<#{in_tag_str}(\s|(\s+.+?=(".+?"|'.+?')))*?>/
-          depth += 1
-        elsif line =~ /<\/#{in_tag_str}\s*>/
-          depth -= 1
-          in_tag = nil if depth == 0
-        end
-      else
-        found_tag = false
-        tags_to_remove.each_with_index do |tag,index|
-          if line =~ /<#{tag}(\s|(\s+.+?=(".+?"|'.+?')))*?>/
-            in_tag = index
-            depth = 1
-            found_tag = true
-            break
-          elsif line =~ /<#{tag}(\s|(\s+.+?=(".+?"|'.+?')))*?\/\s*>/
-            found_tag = true
-            break
-          end
-        end
-        output.write line unless found_tag
+      each_tag do |tag, type|
+        process_tag tag, type
+      end
+
+      if @opts[:close_streams]
+        @input.close
+        @output.close
       end
     end
 
-    if opts[:close_streams]
-      input.close
-      output.close
-    end
+    private
+      def process_tag tag, type
+        if @in_tag
+          in_tag_str = @tags_to_remove[@in_tag]
+
+          if tag =~ opening_tag(in_tag_str)
+            @depth += 1
+          elsif tag =~ closing_tag(in_tag_str)
+            @depth -= 1
+            @in_tag = nil if @depth == 0
+          end
+        else
+          found_tag = false
+          @tags_to_remove.each_with_index do |tag_str,index|
+            if tag =~ opening_tag(tag_str)
+              @in_tag = index
+              @depth = 1
+              found_tag = true
+              break
+            elsif tag =~ single_tag(tag_str)
+              found_tag = true
+              break
+            end
+          end
+
+          @output.write "#{tag}\n" unless found_tag || tag.empty?
+        end
+      end
+
+      def opening_tag tag
+        /<#{tag}(\s|(\s+.+?=(".+?"|'.+?')))*?>/
+      end
+
+      def closing_tag tag
+        /<\/#{tag}\s*>/
+      end
+
+      def single_tag tag
+        /<#{tag}(\s|(\s+.+?=(".+?"|'.+?')))*?\/\s*>/
+      end
+
+      def each_tag &block
+        acc = ""
+        type = nil
+
+        @input.each_char do |c|
+          case c
+          when '<'
+            yield acc.strip, type if type == :text
+            type = :tag
+            acc = c
+          when '>'
+            acc += c
+            yield acc.strip, type
+            type = nil
+            acc = ""
+          else
+            type ||= :text
+            acc += c
+          end
+        end
+
+        yield acc.strip, type if type
+      end
+  end
+
+  def self.process input, output, opts = {}
+    Worker.new(input, output, opts).perform
   end
 
 end
--- spec/tag_remover_spec.rb
+++ spec/tag_remover_spec.rb
@@ -3,72 +3,89 @@ require 'spec_helper'
 describe TagRemover do
   describe ".process" do
     it "removes elements" do
-      input = StringIO.new """
-      <root>
-        <remove>
-          Some contents
-        </remove>
-        <remove >
-        </remove >
-      </root>
-      """
+      input = """
+<root>
+  <remove>
+    Some contents
+  </remove>
+  <remove >
+  </remove >
+</root>"""
+
       output = StringIO.new
       tags_to_remove = ['remove']
 
       TagRemover.process input, output, remove_tags: tags_to_remove
 
-      expect(output.string).to eq """
-      <root>
-      </root>
-      """
+      expect(output.string).to eq """<root>
+</root>
+"""
     end
 
     it "removes single tags" do
-      input = StringIO.new """
-      <root>
-        <remove/>
-        <remove />
-        <remove/ >
-        <remove / >
-      </root>
-      """
+      input = StringIO.new """<root>
+  <remove/>
+  <remove />
+  <remove/ >
+  <remove / >
+</root>
+"""
       output = StringIO.new
       tags_to_remove = ['remove']
 
       TagRemover.process input, output, remove_tags: tags_to_remove
 
-      expect(output.string).to eq """
-      <root>
-      </root>
-      """
+      expect(output.string).to eq """<root>
+</root>
+"""
     end
 
     it "removes tags with attributes" do
-      input = StringIO.new '''
-      <root>
-        <remove x="y" a="b" >
-        </remove>
-        <remove x="y" a="b" />
-      </root>
-      '''
+      input = StringIO.new '''<root>
+  <remove x="y" a="b" >
+  </remove>
+  <remove x="y" a="b" />
+</root>
+'''
       output = StringIO.new
       tags_to_remove = ['remove']
 
       TagRemover.process input, output, remove_tags: tags_to_remove
 
-      expect(output.string).to eq """
-      <root>
-      </root>
-      """
+      expect(output.string).to eq """<root>
+</root>
+"""
+    end
+
+    it "deals with multiple tags on one line" do
+      input = StringIO.new '''<root><remove>
+  </remove>
+  <remove/><keep>
+  </keep>
+  <remove>Bad Stuff!
+  </remove>Happy Stuff :)<keep/>
+</root>
+'''
+      output = StringIO.new
+      tags_to_remove = ['remove']
+
+      TagRemover.process input, output, remove_tags: tags_to_remove
+
+      expect(output.string).to eq """<root>
+<keep>
+</keep>
+Happy Stuff :)
+<keep/>
+</root>
+"""
     end
 
     it "keeps elements" do
-      input = StringIO.new """
-      <root>
-        <keep>
-        </keep>
-      </root>
-      """
+      input = StringIO.new """<root>
+<keep>
+</keep>
+</root>
+"""
       tags_to_remove = ['remove']
 
       output = StringIO.new
@@ -79,10 +96,9 @@ describe TagRemover do
     end
 
     it "is ok with doing nothing" do
-      input = StringIO.new """
-      <root>
-      </root>
-      """
+      input = StringIO.new """<root>
+</root>
+"""
       output = StringIO.new
 
       TagRemover.process input, output
@@ -91,23 +107,21 @@ describe TagRemover do
     end
 
     it "removes nested tags" do
-      input = StringIO.new """
-      <root>
-        <remove>
-          <remove>
-          </remove>
-        </remove>
-      </root>
-      """
+      input = StringIO.new """<root>
+  <remove>
+    <remove>
+    </remove>
+  </remove>
+</root>
+"""
       tags_to_remove = ['remove']
       output = StringIO.new
 
       TagRemover.process input, output, remove_tags: tags_to_remove
 
-      expect(output.string).to eq """
-      <root>
-      </root>
-      """
+      expect(output.string).to eq """<root>
+</root>
+"""
     end
 
     it "closes the streams" do
